b91c6987866519998dbc1c76f0584df594ccb010
Anderson Carubelli <1820277+andersoncarubelli@users.noreply.github.comacarubelli@gmail.com>
2024-06-19 14:53:24 -0300

    improvements for error handling
    Signed-off-by: Anderson Carubelli <1820277+andersoncarubelli@users.noreply.github.comacarubelli@gmail.com>

--- internal/handler/userhandler/user_handler.go
+++ internal/handler/userhandler/user_handler.go
@@ -63,7 +63,7 @@ func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// Create user
+// Update user
 //
 //	@Summary		Create new user
 //	@Description	Endpoint for create user
@@ -124,7 +124,14 @@ func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request) {
 	err = h.service.UpdateUser(r.Context(), req, id)
 	if err != nil {
 		slog.Error(fmt.Sprintf("error to update user: %v", err), slog.String("package", "userhandler"))
-		w.WriteHeader(http.StatusInternalServerError)
+		if err.Error() == "user not found" {
+			w.WriteHeader(http.StatusNotFound)
+			msg := httperr.NewNotFoundError("user not found")
+			json.NewEncoder(w).Encode(msg)
+			return
+		}
+
+		w.WriteHeader(http.StatusBadRequest)
 		msg := httperr.NewBadRequestError("error to update user")
 		json.NewEncoder(w).Encode(msg)
 		return
@@ -167,7 +174,14 @@ func (h *handler) GetUserByID(w http.ResponseWriter, r *http.Request) {
 	res, err := h.service.GetUserByID(r.Context(), id)
 	if err != nil {
 		slog.Error(fmt.Sprintf("error to get user: %v", err), slog.String("package", "userhandler"))
-		w.WriteHeader(http.StatusInternalServerError)
+		if err.Error() == "user not found" {
+			w.WriteHeader(http.StatusNotFound)
+			msg := httperr.NewNotFoundError("user not found")
+			json.NewEncoder(w).Encode(msg)
+			return
+		}
+
+		w.WriteHeader(http.StatusBadRequest)
 		msg := httperr.NewBadRequestError("error to get user")
 		json.NewEncoder(w).Encode(msg)
 		return
@@ -215,6 +229,12 @@ func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request) {
 	err = h.service.DeleteUser(r.Context(), id)
 	if err != nil {
 		slog.Error(fmt.Sprintf("error to delete user: %v", err), slog.String("package", "userhandler"))
+		if err.Error() == "user not found" {
+			w.WriteHeader(http.StatusNotFound)
+			msg := httperr.NewNotFoundError("user not found")
+			json.NewEncoder(w).Encode(msg)
+			return
+		}
 		w.WriteHeader(http.StatusBadRequest)
 		msg := httperr.NewBadRequestError("error to delete user")
 		json.NewEncoder(w).Encode(msg)
@@ -315,7 +335,14 @@ func (h *handler) UpdateUserPassword(w http.ResponseWriter, r *http.Request) {
 	err = h.service.UpdateUserPassword(r.Context(), &req, id)
 	if err != nil {
 		slog.Error(fmt.Sprintf("error to update user password: %v", err), slog.String("package", "userhandler"))
-		w.WriteHeader(http.StatusInternalServerError)
+		if err.Error() == "user not found" {
+			w.WriteHeader(http.StatusNotFound)
+			msg := httperr.NewNotFoundError("user not found")
+			json.NewEncoder(w).Encode(msg)
+			return
+		}
+
+		w.WriteHeader(http.StatusBadRequest)
 		msg := httperr.NewBadRequestError("error to update user password")
 		json.NewEncoder(w).Encode(msg)
 		return
