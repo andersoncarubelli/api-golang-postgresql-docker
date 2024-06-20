e3553b1b6b4d4599b74a3a5f7344c470aaf0f353
Anderson Carubelli <1820277+andersoncarubelli@users.noreply.github.comacarubelli@gmail.com>
2024-06-20 17:12:14 -0300

    add log middleware
    Signed-off-by: Anderson Carubelli <1820277+andersoncarubelli@users.noreply.github.comacarubelli@gmail.com>

--- internal/handler/routes/user_routes.go
+++ internal/handler/routes/user_routes.go
@@ -2,22 +2,25 @@ package routes
 
 import (
 	"github.com/andersoncarubelli/api-golang-postgresql-docker/config/env"
+	"github.com/andersoncarubelli/api-golang-postgresql-docker/internal/handler/middleware"
 	"github.com/andersoncarubelli/api-golang-postgresql-docker/internal/handler/userhandler"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/jwtauth"
 )
 
 func InitUserRoutes(router chi.Router, h userhandler.UserHandler) {
+	router.Use(middleware.LoggerData)
+
 	router.Post("/user", h.CreateUser)
 	router.Route("/user", func(r chi.Router) {
 		r.Use(jwtauth.Verifier(env.Env.TokenAuth))
 		r.Use(jwtauth.Authenticator)
 
-		r.Patch("/{id}", h.UpdateUser)
-		r.Get("/{id}", h.GetUserByID)
-		r.Delete("/{id}", h.DeleteUser)
-		r.Get("/", h.FindManyUsers)
-		r.Patch("/password/{id}", h.UpdateUserPassword)
+		r.Patch("/", h.UpdateUser)
+		r.Get("/", h.GetUserByID)
+		r.Delete("/", h.DeleteUser)
+		r.Get("/list-all", h.FindManyUsers)
+		r.Patch("/password/", h.UpdateUserPassword)
 	})
 	router.Route("/auth", func(r chi.Router) {
 		r.Post("/login", h.Login)
