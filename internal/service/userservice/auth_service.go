051c78d540edd5b523a27c69686b43f5a0904368
Anderson Carubelli <1820277+andersoncarubelli@users.noreply.github.comacarubelli@gmail.com>
2024-06-25 15:21:15 -0300

    change method parameter type
    Signed-off-by: Anderson Carubelli <1820277+andersoncarubelli@users.noreply.github.comacarubelli@gmail.com>

--- internal/service/userservice/auth_service.go
+++ internal/service/userservice/auth_service.go
@@ -31,7 +31,7 @@ func (s *service) Login(ctx context.Context, u dto.LoginDTO) (*response.UserAuth
 		return nil, errors.New("error to search user password")
 	}
 
-	err = bcrypt.CompareHashAndPassword([]byte(userPass.Password), []byte(u.Password))
+	err = bcrypt.CompareHashAndPassword([]byte(userPass), []byte(u.Password))
 	if err != nil {
 		slog.Error("invalid password", slog.String("package", "userservice"))
 		return nil, errors.New("invalid password")
